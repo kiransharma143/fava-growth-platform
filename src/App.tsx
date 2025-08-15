e7cc644063e87ceacaa6e97480c1c0917156db61
kiran <kiran84217@gmail.com>
2025-08-16 00:56:43 +0530

    image chnages

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/layout/Header";
 import Footer from "./components/layout/Footer";
-import Home from "./pages/Home";
+import Home from "../public/Home";
 import NotFound from "./pages/NotFound";
 import TopContactBar from "./components/ui/TopContactbar";
 
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,5 @@
 
-import Home from "./Home";
+import Home from "../../public/Home";
 
 const Index = () => {
   return <Home />;
